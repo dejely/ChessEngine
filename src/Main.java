1b7759eebfdee5a8655f4a7fc1ce8c2ce83c1787
dejely <captain.jelly@yahoo.com>
2025-11-07 01:27:05 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
+import chess.Game;
 
 public class Main {
 
-	public Main() {
-		// TODO Auto-generated constructor stub
-	}
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//__init__ game
+		
+		Game game = new Game();
+		
 	}
 
 }
